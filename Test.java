711b2271aa96f2d1a3975892aba5415aa830a5fa
Jacob <jw.braidley@gmail.com>
2022-05-06 17:27:51 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,34 +9,33 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class Test {
-    public static void main ( String[] args )
-{
-    JPanel middlePanel = new JPanel ();
-    middlePanel.setBorder ( new TitledBorder ( new EtchedBorder (), "Display Area" ) );
+    public static void main(String[] args) {
+        JPanel middlePanel = new JPanel();
+        middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "Display Area"));
 
-    JTextArea display = new JTextArea ( 16, 58 );
-    display.setEditable ( false ); // set textArea non-editable
-    JScrollPane scroll = new JScrollPane ( display );
-    scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        JTextArea display = new JTextArea(16, 58);
+        display.setEditable(false); // set textArea non-editable
+        JScrollPane scroll = new JScrollPane(display);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-    JTextField txt = new JTextField(10);
-    middlePanel.add(txt);
+        JTextField txt = new JTextField(10);
+        middlePanel.add(txt);
 
-    JButton btn = new JButton("submit");
-    btn.addActionListener(e -> {
-        display.append(txt.getText());
-    });
-    middlePanel.add(btn);
+        JButton btn = new JButton("submit");
+        btn.addActionListener(e -> {
+            display.append(txt.getText() + "\n");
+        });
+        middlePanel.add(btn);
 
 
-    //Add Textarea in to middle panel
-    middlePanel.add ( scroll );
+        //Add Textarea in to middle panel
+        middlePanel.add(scroll);
 
-    // My code
-    JFrame frame = new JFrame ();
-    frame.add ( middlePanel );
-    frame.pack ();
-    frame.setLocationRelativeTo ( null );
-    frame.setVisible ( true );
-}
+        // My code
+        JFrame frame = new JFrame ();
+        frame.add(middlePanel);
+        frame.pack();
+        frame.setLocationRelativeTo (null);
+        frame.setVisible(true);
+    }
 }
