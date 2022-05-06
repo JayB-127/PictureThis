f209e3b01db5519daaf0268430ae33532e5f0791
Jacob <jw.braidley@gmail.com>
2022-05-06 18:45:22 +0100

    FULLY WORKING CHAT AREA METHOD

--- GuessingPhase.java
+++ GuessingPhase.java
@@ -1,27 +1,26 @@
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 import java.awt.Container;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
-
 import java.awt.Dimension;
 
 import java.awt.Color;
-import java.awt.Component;
 
 public class GuessingPhase{
     
     JFrame guessingFrame = new JFrame("Picture This! - Guessing Phase");
-    JButton submitBtn;
-    JTextField inputTxt;
-    JTextArea chatArea;
+    private JButton submitBtn;
+    private JTextField inputTxt;
+    private JTextArea chatArea;
+    private JScrollPane scroll;
     
     public void show() {
 
@@ -30,36 +29,13 @@ public class GuessingPhase{
 
         JComponent inputs = inputs();
         content.add(inputs, BorderLayout.SOUTH);
-
-
-        chatArea = new JTextArea(11, 30);
-        chatArea.setFont(chatArea.getFont().deriveFont(15.0f));
-        chatArea.setLineWrap(true);
-        chatArea.setWrapStyleWord(true);
-
-        JScrollPane scroll = new JScrollPane(
-            chatArea,
-            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
-        );
-
-        content.add(chatArea);
-
-
-
-        JPanel display = new JPanel();
-        display.setBackground(Color.BLUE);
-        display.setLayout(new FlowLayout());
-        display.add(chatArea);
-        display.add(scroll);
-
-        content.add(display, BorderLayout.CENTER);
-
         
+        JComponent display = display();
+        content.add(display, BorderLayout.CENTER);
 
         JPanel pics = new JPanel();
         pics.setPreferredSize(new Dimension(0, 300));
-        pics.setBackground(Color.RED);
+        pics.setBackground(Color.GRAY);
         content.add(pics, BorderLayout.NORTH);
 
 
@@ -69,39 +45,47 @@ public class GuessingPhase{
         guessingFrame.setResizable(false);
         guessingFrame.setLocationRelativeTo(null);
 
+        guessingFrame.getRootPane().setDefaultButton(submitBtn);
+
     }
 
     private JComponent inputs() {
         JPanel inputs = new JPanel();
-        inputs.setBackground(Color.decode("#ABCDEF"));
-        inputs.setPreferredSize(new Dimension(0, 40));
+        inputs.setLayout(new FlowLayout());
+        inputs.setPreferredSize(new Dimension(0, 50));
 
-        GridLayout gridLay = new GridLayout(1, 0);
-        gridLay.setHgap(5);
-
-        inputs.setLayout(gridLay);
-
-
-
-        inputTxt = new JTextField(5);
+        inputTxt = new JTextField(30);
         inputTxt.setFont(inputTxt.getFont().deriveFont(20.0f));
         inputTxt.setHorizontalAlignment(JTextField.CENTER);
-        inputTxt.setAlignmentY(Component.CENTER_ALIGNMENT);
-
         inputs.add(inputTxt);
        
-
         submitBtn = new JButton("Submit");
         submitBtn.addActionListener(e -> {
-            System.out.println("[Submitted]");
-            //check if text area has a certain amount of lines, if too many, remove top line
+            //TODO: validation
             chatArea.append(inputTxt.getText() + "\n");
+            inputTxt.setText("");
         });
-        submitBtn.setAlignmentY(Component.CENTER_ALIGNMENT);
-
+        submitBtn.setPreferredSize(new Dimension(200, 30));
         inputs.add(submitBtn);
 
         return inputs;
+    }
+
+    private JComponent display() {
+        JPanel display = new JPanel();
+
+        chatArea = new JTextArea(12, 45);
+        chatArea.setFont(chatArea.getFont().deriveFont(20.0f));
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        chatArea.setWrapStyleWord(true);
+
+        scroll = new JScrollPane(chatArea);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        display.add(scroll);
+
+        return display;
     }
 
     public static void main(String[] args) {
